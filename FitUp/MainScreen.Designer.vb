f1b7dcebd68e83c185382e98f6d666d405cb314d
catalindumitrache76 <catalindumitrache76@yahoo.es>
2017-05-27 22:54:51 +0200

    Prepared database and added "Add Meal Section"

--- FitUp/MainScreen.Designer.vb
+++ FitUp/MainScreen.Designer.vb
@@ -26,26 +26,47 @@ Partial Class MainScreen
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.TabControl3 = New System.Windows.Forms.TabControl()
+        Me.TabPage5 = New System.Windows.Forms.TabPage()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.TabPage6 = New System.Windows.Forms.TabPage()
+        Me.SplitContainer3 = New System.Windows.Forms.SplitContainer()
+        Me.AddAMealButton = New System.Windows.Forms.Button()
+        Me.TabPage7 = New System.Windows.Forms.TabPage()
         Me.TabControl2 = New System.Windows.Forms.TabControl()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
         Me.TabPage4 = New System.Windows.Forms.TabPage()
-        Me.TabControl3 = New System.Windows.Forms.TabControl()
-        Me.TabPage5 = New System.Windows.Forms.TabPage()
-        Me.TabPage6 = New System.Windows.Forms.TabPage()
-        Me.TabPage7 = New System.Windows.Forms.TabPage()
+        Me.BaseDatosFitUpDataSet1 = New FitUp.BaseDatosFitUpDataSet()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer2.Panel1.SuspendLayout()
         Me.SplitContainer2.Panel2.SuspendLayout()
         Me.SplitContainer2.SuspendLayout()
-        Me.TabControl2.SuspendLayout()
         Me.TabControl3.SuspendLayout()
+        Me.TabPage5.SuspendLayout()
+        Me.TabPage6.SuspendLayout()
+        CType(Me.SplitContainer3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer3.Panel1.SuspendLayout()
+        Me.SplitContainer3.SuspendLayout()
+        Me.TabControl2.SuspendLayout()
+        CType(Me.BaseDatosFitUpDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'SplitContainer1
@@ -82,7 +103,8 @@ Partial Class MainScreen
         '
         'TabPage1
         '
-        Me.TabPage1.BackColor = System.Drawing.Color.Aquamarine
+        Me.TabPage1.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.TabPage1.Controls.Add(Me.TableLayoutPanel1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 31)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
@@ -90,9 +112,92 @@ Partial Class MainScreen
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Meals of the day"
         '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Label8, 0, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label7, 0, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label6, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label4, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label5, 0, 2)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(408, 680)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(3, 565)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(63, 20)
+        Me.Label8.TabIndex = 5
+        Me.Label8.Text = "Others"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(3, 452)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(63, 20)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Dinner"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(3, 339)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(117, 20)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "5 o'clock p.m."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Breakfast"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(3, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "12 o'clock a.m."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(3, 226)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 20)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Lunch"
+        '
         'TabPage2
         '
-        Me.TabPage2.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.TabPage2.BackColor = System.Drawing.Color.MediumTurquoise
         Me.TabPage2.Location = New System.Drawing.Point(4, 31)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
@@ -120,38 +225,6 @@ Partial Class MainScreen
         Me.SplitContainer2.SplitterDistance = 392
         Me.SplitContainer2.TabIndex = 0
         '
-        'TabControl2
-        '
-        Me.TabControl2.Controls.Add(Me.TabPage3)
-        Me.TabControl2.Controls.Add(Me.TabPage4)
-        Me.TabControl2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TabControl2.Font = New System.Drawing.Font("Rockwell", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TabControl2.Location = New System.Drawing.Point(0, 0)
-        Me.TabControl2.Name = "TabControl2"
-        Me.TabControl2.SelectedIndex = 0
-        Me.TabControl2.Size = New System.Drawing.Size(510, 721)
-        Me.TabControl2.TabIndex = 0
-        '
-        'TabPage3
-        '
-        Me.TabPage3.BackColor = System.Drawing.Color.DarkOrange
-        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage3.Name = "TabPage3"
-        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage3.Size = New System.Drawing.Size(465, 632)
-        Me.TabPage3.TabIndex = 0
-        Me.TabPage3.Text = "Exercises of the day"
-        '
-        'TabPage4
-        '
-        Me.TabPage4.BackColor = System.Drawing.Color.SandyBrown
-        Me.TabPage4.Location = New System.Drawing.Point(4, 31)
-        Me.TabPage4.Name = "TabPage4"
-        Me.TabPage4.Padding = New System.Windows.Forms.Padding(10)
-        Me.TabPage4.Size = New System.Drawing.Size(502, 686)
-        Me.TabPage4.TabIndex = 1
-        Me.TabPage4.Text = "Exercises of the week"
-        '
         'TabControl3
         '
         Me.TabControl3.Controls.Add(Me.TabPage5)
@@ -167,7 +240,10 @@ Partial Class MainScreen
         '
         'TabPage5
         '
-        Me.TabPage5.BackColor = System.Drawing.Color.Goldenrod
+        Me.TabPage5.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.TabPage5.Controls.Add(Me.Label2)
+        Me.TabPage5.Controls.Add(Me.Label1)
+        Me.TabPage5.Controls.Add(Me.DateTimePicker1)
         Me.TabPage5.Location = New System.Drawing.Point(4, 31)
         Me.TabPage5.Name = "TabPage5"
         Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
@@ -175,9 +251,39 @@ Partial Class MainScreen
         Me.TabPage5.TabIndex = 0
         Me.TabPage5.Text = "Calendar"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Exercises:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Meals:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(3, 3)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(378, 27)
+        Me.DateTimePicker1.TabIndex = 2
+        '
         'TabPage6
         '
-        Me.TabPage6.BackColor = System.Drawing.Color.DarkGoldenrod
+        Me.TabPage6.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.TabPage6.Controls.Add(Me.SplitContainer3)
         Me.TabPage6.Location = New System.Drawing.Point(4, 31)
         Me.TabPage6.Name = "TabPage6"
         Me.TabPage6.Padding = New System.Windows.Forms.Padding(3)
@@ -185,20 +291,83 @@ Partial Class MainScreen
         Me.TabPage6.TabIndex = 1
         Me.TabPage6.Text = "My meals"
         '
+        'SplitContainer3
+        '
+        Me.SplitContainer3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer3.Location = New System.Drawing.Point(3, 3)
+        Me.SplitContainer3.Name = "SplitContainer3"
+        Me.SplitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer3.Panel1
+        '
+        Me.SplitContainer3.Panel1.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.SplitContainer3.Panel1.Controls.Add(Me.AddAMealButton)
+        Me.SplitContainer3.Size = New System.Drawing.Size(378, 680)
+        Me.SplitContainer3.SplitterDistance = 37
+        Me.SplitContainer3.TabIndex = 0
+        '
+        'AddAMealButton
+        '
+        Me.AddAMealButton.Dock = System.Windows.Forms.DockStyle.Top
+        Me.AddAMealButton.Font = New System.Drawing.Font("Rockwell", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddAMealButton.Location = New System.Drawing.Point(0, 0)
+        Me.AddAMealButton.Name = "AddAMealButton"
+        Me.AddAMealButton.Size = New System.Drawing.Size(378, 34)
+        Me.AddAMealButton.TabIndex = 1
+        Me.AddAMealButton.Text = "Add a meal"
+        Me.AddAMealButton.UseVisualStyleBackColor = True
+        '
         'TabPage7
         '
-        Me.TabPage7.BackColor = System.Drawing.Color.Gold
+        Me.TabPage7.BackColor = System.Drawing.Color.MediumTurquoise
         Me.TabPage7.Location = New System.Drawing.Point(4, 31)
         Me.TabPage7.Name = "TabPage7"
         Me.TabPage7.Size = New System.Drawing.Size(384, 686)
         Me.TabPage7.TabIndex = 2
         Me.TabPage7.Text = "My exercises"
         '
+        'TabControl2
+        '
+        Me.TabControl2.Controls.Add(Me.TabPage3)
+        Me.TabControl2.Controls.Add(Me.TabPage4)
+        Me.TabControl2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl2.Font = New System.Drawing.Font("Rockwell", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabControl2.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl2.Name = "TabControl2"
+        Me.TabControl2.SelectedIndex = 0
+        Me.TabControl2.Size = New System.Drawing.Size(510, 721)
+        Me.TabControl2.TabIndex = 0
+        '
+        'TabPage3
+        '
+        Me.TabPage3.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.TabPage3.Location = New System.Drawing.Point(4, 31)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(502, 686)
+        Me.TabPage3.TabIndex = 0
+        Me.TabPage3.Text = "Exercises of the day"
+        '
+        'TabPage4
+        '
+        Me.TabPage4.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.TabPage4.Location = New System.Drawing.Point(4, 31)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(10)
+        Me.TabPage4.Size = New System.Drawing.Size(502, 686)
+        Me.TabPage4.TabIndex = 1
+        Me.TabPage4.Text = "Exercises of the week"
+        '
+        'BaseDatosFitUpDataSet1
+        '
+        Me.BaseDatosFitUpDataSet1.DataSetName = "BaseDatosFitUpDataSet"
+        Me.BaseDatosFitUpDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
         'MainScreen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
         Me.ClientSize = New System.Drawing.Size(1332, 721)
         Me.Controls.Add(Me.SplitContainer1)
         Me.ForeColor = System.Drawing.SystemColors.ControlText
@@ -213,12 +382,22 @@ Partial Class MainScreen
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.SplitContainer2.Panel1.ResumeLayout(False)
         Me.SplitContainer2.Panel2.ResumeLayout(False)
         CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer2.ResumeLayout(False)
-        Me.TabControl2.ResumeLayout(False)
         Me.TabControl3.ResumeLayout(False)
+        Me.TabPage5.ResumeLayout(False)
+        Me.TabPage5.PerformLayout()
+        Me.TabPage6.ResumeLayout(False)
+        Me.SplitContainer3.Panel1.ResumeLayout(False)
+        CType(Me.SplitContainer3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer3.ResumeLayout(False)
+        Me.TabControl2.ResumeLayout(False)
+        CType(Me.BaseDatosFitUpDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -235,4 +414,17 @@ Partial Class MainScreen
     Friend WithEvents TabPage5 As TabPage
     Friend WithEvents TabPage6 As TabPage
     Friend WithEvents TabPage7 As TabPage
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents SplitContainer3 As SplitContainer
+    Friend WithEvents AddAMealButton As Button
+    Friend WithEvents BaseDatosFitUpDataSet1 As BaseDatosFitUpDataSet
 End Class
--- FitUp/MainScreen.vb
+++ FitUp/MainScreen.vb
@@ -1,5 +1,37 @@
-﻿Public Class MainScreen
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class MainScreen
+
+    Dim AddAMealDoneButton As New Button
+    Dim MealDescriptionTextbox As New TextBox
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+
     Private Sub MainScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SplitContainer3.SplitterDistance = AddAMealButton.Height
+        ConnectToDatabase()
+    End Sub
+
+    Private Sub ConnectToDatabase()
+
+
+        '' FIND INFO HERE: https://msdn.microsoft.com/en-us/library/ms233812.aspx
+        ''Create a New row.
+        'Dim newRegionRow As NorthwindDataSet.RegionRow
+        'newRegionRow = Me.NorthwindDataSet._Region.NewRegionRow()
+        'newRegionRow.RegionID = 5
+        'newRegionRow.RegionDescription = "NorthWestern"
+
+        '' Add the row to the Region table
+        'Me.NorthwindDataSet._Region.Rows.Add(newRegionRow)
+
+        '' Save the new row to the database
+        'Me.RegionTableAdapter.Update(Me.NorthwindDataSet._Region)
+
+
+
 
     End Sub
 
@@ -7,10 +39,61 @@
 
         Dim myControl As Control
         myControl = sender
+        'If MyBase.MinimizeBox Then
+        'Else
 
         SplitContainer1.SplitterDistance = myControl.Size.Width / 3
         SplitContainer2.SplitterDistance = myControl.Size.Width / 3
+        MealDescriptionTextbox.Width = (myControl.Size.Width / 3) - 6
+        Dim AddAMealDoneButtonLocation = New Point(
+            MealDescriptionTextbox.Location.X, SplitContainer3.SplitterDistance - AddAMealButton.Height)
+        MealDescriptionTextbox.Height = SplitContainer3.SplitterDistance - (AddAMealButton.Height * 2)
+
 
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles AddAMealButton.Click
+
+        'Expand the container and add a textbox with a button
+        SplitContainer3.SplitterDistance = 150
+        MealDescriptionTextbox.Font = AddAMealButton.Font
+        MealDescriptionTextbox.Multiline = True
+
+        AddAMealDoneButton.Dock = DockStyle.Bottom
+        AddAMealDoneButton.Size = AddAMealButton.Size
+        AddAMealDoneButton.Font = AddAMealButton.Font
+        AddAMealDoneButton.BackColor = Color.LightGreen
+        AddAMealDoneButton.Text = "Done"
+        Dim MealDescriptionLocation = New Point(AddAMealButton.Location.X, AddAMealButton.Location.Y + AddAMealButton.Height)
+        MealDescriptionTextbox.Location = MealDescriptionLocation
+        MealDescriptionTextbox.Size = New Size(AddAMealButton.Width, 150 - (AddAMealButton.Height * 2))
+
+        SplitContainer3.Panel1.Controls.Add(MealDescriptionTextbox)
+        SplitContainer3.Panel1.Controls.Add(AddAMealDoneButton)
+
+        AddHandler AddAMealDoneButton.Click, AddressOf AddAMealDoneButton_Click
+
+    End Sub
+
+    Private Sub AddAMealDoneButton_Click(sender As Object, e As EventArgs)
+        If MealDescriptionTextbox.Text Is "" Then
+            MsgBox("You have to provide a description for the meal")
+        Else
+            SplitContainer3.Panel1.Controls.Remove(MealDescriptionTextbox)
+            MealDescriptionTextbox = New TextBox
+            SplitContainer3.Panel1.Controls.Remove(AddAMealDoneButton)
+            AddAMealDoneButton = New Button
+            SplitContainer3.SplitterDistance = AddAMealButton.Height
+            'Create a Command object.
+            myCmd = myConn.CreateCommand
+
+            myCmd.CommandText = "INSERT INTO Meal (name, category, state) 
+                VALUES (" + MealDescriptionTextbox.Text + ", ""Breakfast"", ""planned"");"
+
+            myCmd.ExecuteNonQuery()
+
+
+            MsgBox("Your meal has been saved.")
+        End If
+    End Sub
 End Class
